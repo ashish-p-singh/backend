8ca83154451eb096dee7300ee1f7805dd79205b6
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-07-02 22:17:41 +0530

    Refactor main.js to use require for module imports and enhance file operations with fs

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ import {a,b}  from "./module.js"
 console.log(a)
 import ash from "./module.js"
 console.log(ash)
-// let a=require("./module.js")
-// console.log(a,__dirname,__filename)
-import fs from fs
+let a=require("./module.js")
+console.log(a,__dirname,__filename)
+//---------------------------------------------
+const fs= require('fs')
+console.log('Starting')
+fs.writeFile('Ash2.txt','how are you2',()=>{
+    console.log('done')
+    fs.readFile('Ash2.txt',(error,data)=>{
+        console.log(data.toString())
+    })
+})
+fs.appendFile('Ash.txt','Ashish')
+console.log('Ending')
+//----------------------------------------
+import fs from "fs/promises"
+let a=await fs.readFile("Ash.txt")
+console.log(a.toString())
