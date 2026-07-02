cf04184cf9b1a05bdb9dff57133ae742abba5d6a
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-07-02 10:13:15 +0530

    Add main.js and module.js with initial exports and console logs

---
+++ main.js
@@ -0,0 +1,7 @@
+import {a,b}  from "./module.js"
+console.log(a)
+import ash from "./module.js"
+console.log(ash)
+// let a=require("./module.js")
+// console.log(a,__dirname,__filename)
+import fs from fs
---
+++ module.js
@@ -0,0 +1,12 @@
+export let a=5
+export let b=5
+export let c=5
+let obj={
+    a:5,
+    b:3
+}
+export default obj
+// module.exports={
+// a:5,
+// b:3,
+// }
