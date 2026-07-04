193551f622f70149bb52e42d9c6ff83a55a9c266
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-07-05 00:01:42 +0530

    Update server port configuration to use environment variable and enhance startup message

--- ejs.js
+++ ejs.js
@@ -1,6 +1,6 @@
 import express from 'express'
 let app=express()
-const port=3000;
+const port=process.env.PORT;
 app.set("view engine","ejs")
 app.get('/',(req,res)=>{
     let siteName="Ashish"
@@ -8,5 +8,5 @@ app.get('/',(req,res)=>{
     res.render("index",{siteName:siteName,searchText:searchText})
 })
 app.listen(port,()=>{
-    console.log("Server started")
+    console.log(`Server running on http://localhost:${port}`)
 })
