2c230319e508bee42454c20284c37def33c3c3fc
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-07-04 14:45:20 +0530

    Add express.js for server setup and create initial public files

---
+++ express.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/contact', (req, res) => {
+  res.send('Hello contact!');
+});
+app.get('/blog/:slug', (req, res) => {
+      console.log(req.params)
+  res.send(`Hello ${req.params.slug}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
