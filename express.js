4801074adf9061d2d8f2a30773403b722471d59e
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-07-09 01:07:30 +0530

    Refactor index.html: simplify structure and update styles for glowing bulb feature

--- express.js
+++ express.js
@@ -1,28 +1,33 @@
-import express from 'express';
-import blog from "./routes/blog.js"
-import fs from 'fs';
+import express from "express";
+import blog from "./routes/blog.js";
+import fs from "fs";
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-  fs.appendFileSync("log.txt", `The type of request on ${new Date().toLocaleString("en-IN")} is a ${req.method}\n`)
-  next()
-})
-app.use(express.static('public'))
-app.use("/blog", blog)
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+  fs.appendFileSync(
+    "log.txt",
+    `The type of request on ${new Date().toLocaleString("en-IN")} is a ${req.method}\n`,
+  );
+  next();
 });
-app.get('/contact', (req, res) => {
-  res.send('Hello contact!');
+app.use(express.static("public"));
+app.use("/blog", blog);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/contact", (req, res) => {
+  res.send("Hello contact!");
 });
 
 app.get("/index", (req, res) => {
-  res.sendFile("C:\\Users\\techn\\OneDrive\\Documents\\vs code 2\\backend\\templates\\index.html")
-})
-app.post('/', (req, res) => {
-  res.send("This is post request")
-})
+  res.sendFile(
+    "C:\\Users\\techn\\OneDrive\\Documents\\vs code 2\\backend\\templates\\index.html",
+  );
+});
+app.post("/", (req, res) => {
+  res.send("This is post request");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
